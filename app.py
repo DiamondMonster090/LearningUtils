389f868efadcb42d48d25e09ee3dfc891014335e
eytangut <eytangut@gmail.com>
2024-06-13 18:38:01 +0300

    added flask session and phpLiteAdmin. also added a jumbotron for index

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
-
+from flask_session import Session
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def index():
